5f257e56e4bbae269d7652b7b335fb23aa04f5dd
edumgt <kimdypm@gmail.com>
2025-09-09 14:50:05 +0900

    update

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,25 @@
+
+
+/* 소스 정보 
+ * 작성자: 홍
+ * 작성일: yyyymmdd
+ * 
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        int v1 = 15;
+        if (v1 > 10) {
+            int v2;
+            v2 = v1 - 10;
+
+            System.out.println("1번영역의 v2 " + v2);
+        }
+        int v2 = 10;
+        int v3 = v1 + v2 + 5;
+
+        System.out.println("2번영역의 v2 " + v2);
+
+    }
+}

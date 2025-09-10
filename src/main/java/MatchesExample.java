9d5e8c42c4064b36a9bfb781212175b7de1224ae
edumgt <kimdypm@gmail.com>
2025-09-10 10:55:38 +0900

    추가

--- src/main/java/MatchesExample.java
+++ src/main/java/MatchesExample.java
@@ -1,17 +1,17 @@
 public class MatchesExample {
     public static void main(String[] args) {
-        String str1 = "Hello123";
-        String str2 = "12345";
-        String str3 = "abc";
+        String str1 = "123";
+        String str2 = "!@#$%";
+        String str3 = "abc123";
 
         // 영문자 + 숫자 조합인지 확인 (전체 검사)
-        System.out.println(str1.matches("[A-Za-z0-9]+")); // true
-        System.out.println(str2.matches("[A-Za-z0-9]+")); // true
-        System.out.println(str3.matches("[0-9]+"));       // false
+        // System.out.println(str1.matches("[A-Za-z0-9]+")); // true
+        // System.out.println(str2.matches("[A-Za-z0-9!@#]+")); // true
+        // System.out.println(str3.matches("[0-9]+"));       // false
 
         // 이메일 형식 확인 예시
-        String email = "test@example.com";
-        boolean isEmail = email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+        String email = "testexampl@abcdefg.com";
+        boolean isEmail = email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{5,}$");
         System.out.println("이메일 형식인가요? " + isEmail); // true
     }
 }

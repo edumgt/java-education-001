aef14f48a001f2f3336b4801344506e31a5cf9ef
edumgt <kimdypm@gmail.com>
2025-09-11 11:07:34 +0900

    update

---
+++ src/main/java/Example3.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Example3 {
+    public static void main(String[] args) throws IOException  {
+        int keyCode = System.in.read();
+        System.out.println("keyCode -- " + keyCode);
+    }
+}

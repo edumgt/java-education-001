925b2de44f38392c18091aacf688d15d3b484bd6
edumgt <kimdypm@gmail.com>
2025-09-12 10:09:39 +0900

    update

---
+++ src/main/java/Example5.java
@@ -0,0 +1,24 @@
+import java.util.UUID;
+
+public class Example5 {
+    public static void main(String[] args) {
+        while (true) {
+            UUID uuid = UUID.randomUUID();
+            String uuidStr = uuid.toString();
+
+            // 앞 2자리 추출
+            String prefix = uuidStr.substring(0, 2);
+
+            // 숫자 여부 확인 (00~99)
+            if (prefix.matches("\\d{2}")) {
+                System.out.println("조건 충족! UUID = " + uuidStr);
+                break; // while 탈출
+            } else {
+                System.out.println("조건 불충족: " + uuidStr);
+            }
+        }
+
+        System.out.println("프로그램 종료");
+    }
+
+}
---
+++ src/main/java/Example6.java
@@ -0,0 +1,14 @@
+public class Example6 {
+    public static void main(String[] args) {
+        outer: 
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if (j == 2) {
+                    System.out.println("j==2 → 바깥 loop 다음으로 넘어감");
+                    continue; // 안쪽 loop를 중단하고 바깥 loop 다음 회차로 이동
+                }
+                System.out.println("i=" + i + ", j=" + j);
+            }
+        }
+    }
+}
